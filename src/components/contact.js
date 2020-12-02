648192baca6a130b897b0419f50351e6b7c262c9
“Nasra <nasra_09@yahoo.com>
2020-12-01 21:25:36 -0600

    Changes

--- src/components/contact.js
+++ src/components/contact.js
@@ -10,7 +10,7 @@ class Contact extends Component {
     <div className="container">
       <div className="row">
         <div className="col-lg-12 text-center">
-          <h2 className="section-heading text-uppercase">Contact Us</h2>
+          <h2 className="section-heading text-uppercase">Contact Me</h2>
           <h3 className="section-subheading text-muted">Say Hello</h3>
         </div>
       </div>
